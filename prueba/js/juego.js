956b7e6e5f28e6f51ef91896fec87f3b9cfc6ffd
JRevillaT <jrevillat@unsa.edu.pe>
2021-02-04 12:21:45 -0500

    Small changes

--- prueba/js/juego.js
+++ prueba/js/juego.js
@@ -1,5 +1,5 @@
 var fps = 10;
-var xEscenario;
+var xEscenario = 0;
 
 function moverEscenario(){
   xEscenario++;
@@ -7,7 +7,8 @@ function moverEscenario(){
 }
 
 function main(){
-  console.log('fotograma');
+  //console.log('fotograma');
+  moverEscenario();
 }
 function atacar(){
   console.log("Has atacado");
